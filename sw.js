c053eaddd71ef07cb21eb13f92d8b3a4aa271801
Anton870-dot <zavolovsky04@gmail.com>
2026-09-24 23:50:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняй номер версии при каждом обновлении игры
-const CACHE_NAME = 'snake-v19';
+const CACHE_NAME = 'snake-v20';
 const urls = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // Установка: кладём файлы в новый кэш и сразу активируемся
